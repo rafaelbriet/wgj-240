031e1bf04d36104644be08e28c8c0f948da9abb5
Rafael Briet <rafael-briet@hotmail.com>
2022-02-12 23:26:55 -0300

    Organize files

--- Assets/__Project/Scripts/TargetShooting/TargetShootingPlayerController.cs
+++ Assets/__Project/Scripts/TargetShooting/TargetShootingPlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class TargetShootingPlayerController : MonoBehaviour
 {
     [SerializeField]
     private Transform crosshair;
